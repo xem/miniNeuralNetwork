bf47f50e02ea64391eaabdc8c608f9101c335970
xem <maxime.euziere@gmail.com>
2024-11-05 15:19:18 +0100

    541b without C()

--- commented.js
+++ commented.js
@@ -7,11 +7,8 @@ g = x => x * (1 - x);
 // Learning rate
 l = 0.2;
 
-// Columnize ([1,2] => [[1],[2]])
-C = a => a.map(z=>[z]);
-
 // Matrix
-M = (r, c, z) => Array(r).fill().map(() => Array(c).fill().map(x=>z?0:Math.random()*2-1));
+M = (r, c, z) => Array(r).fill().map(() => Array(c).fill().map(x => z ?? Math.random() * 2 - 1));
 
 // Operations on matrices
 // o === 0 (default): transpose (a.T)
@@ -23,11 +20,11 @@ M = (r, c, z) => Array(r).fill().map(() => Array(c).fill().map(x=>z?0:Math.rando
 // o === 6: dot (a.b)
 O = (a, b, o, r = a, i, j, k, l = "length", z) => {
   if(!o) r = M(a[0][l], a[l]);
-  if(o > 5) r = M(a[l], b[0][l], 1);
+  if(o > 5) r = M(a[l], b[0][l], 0);
   for(i = r[l]; i--;){
     for(j = r[0][l]; j--;){
       if(o > 5){
-        for(k = a[0][l];k--;){
+        for(k = a[0][l]; k--;){
           r[i][j] += a[i][k] * b[k][j]; // dot
         }
       }
@@ -57,11 +54,11 @@ I = (i, h, o) => {
 // P(input, target) // train
 // P(input) // query
 P = (i, t, o) => {
-  o = O(O(w, h = O(O(W, i = C(i), 6), f, 5), 6), f, 5);
+  o = O(O(w, h = O(O(W, i = O(i), 6), f, 5), 6), f, 5);
   if(t){
-    O(t = C(t), o, 2);
+    O(t = O(t), o, 2);
     O(w, O(O(O(O(o, g, 5), t, 3), l, 4), O(h), 6), 1);
     O(W, O(O(O(O(h, g, 5), O(O(w), t, 6), 3), l, 4), O(i), 6), 1);
   }
-  return o.flat();
+  return o;
 }
