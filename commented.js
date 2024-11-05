81eab7971b400f03c2c63f0c6c04c462697c4188
xem <maxime.euziere@gmail.com>
2024-11-05 08:31:39 +0100

    611b (matrix operations in one place)

--- commented.js
+++ commented.js
@@ -8,28 +8,36 @@ g = x => x * (1 - x);
 l = 0.2;
 
 // Matrix
-M = (rows, cols) => Array(rows).fill().map(() => Array(cols).fill(0));
+M = (r, c, z) => Array(r).fill().map(() => Array(c).fill(0));
 
 // Columnize ([1,2] => [[1],[2]])
 C = a => a.map(z=>[z]);
 
-// Sub
-S = (a, b, r, i, j) => { for(i in a) for(j in a[i]) a[i][j] -= b[i][j]; }
 
-// Transpose
-T = (a, b, r, i, j) => { r = M(a[0].length, a.length); for(i in r) for(j in r[i]) r[i][j] = a[j][i]; return r; }
-
-// Map
-F = (a, b, r, i, j) => { for(i in a) for(j in a[i]) a[i][j] = b(a[i][j]); return a; }
-
-// Dot
-D = (a, b, r, i, j, k) => {
-  r = M(a.length, b[0].length);
-  a.map((z,i)=>{
-    b[0].map((y,j)=>{
-      a[0].map((x,k)=>{
-        r[i][j] += a[i][k] * b[k][j];
-      })
+// Operations on matrices
+// o === 0 (default): transpose (a.T)
+// o === 1: add (a+b)
+// o === 2: sub (a-b)
+// o === 3: mul (a*b)
+// o === 4: scale (a*b)
+// o === 5: map (b(a))
+// o === 6: dot (a.b)
+O = (a, b, o, r = a, i, j, k) => {
+  if(!o) r = M(a[0].length, a.length);
+  if(o === 6) r = M(a.length, b[0].length);
+  r.map((z,i)=>{
+    r[i].map((y,j)=>{
+      if(!o) r[i][j] = a[j][i]; // transpose
+      if(o === 1) a[i][j] += b[i][j]; // add
+      if(o === 2) a[i][j] -= b[i][j]; // sub
+      if(o === 3) a[i][j] *= b[i][j]; // mul
+      if(o === 4) a[i][j] *= b; // scale
+      if(o === 5) a[i][j] = b(a[i][j]) // map
+      if(o === 6){
+        a[0].map((x,k)=>{
+          r[i][j] += a[i][k] * b[k][j]; // dot
+        })
+      }
     })
   })
   return r;
@@ -37,15 +45,6 @@ D = (a, b, r, i, j, k) => {
 
 // Randomize
 R = (a, b, i, j) => { for(i in a) for(j in a[i]) a[i][j] = Math.random() * 2 - 1; return a; }
-
-// Add
-A = (a, b, i, j) => { for(i in a) for(j in a[i]) a[i][j] += b[i][j]; return a; }
-
-// Resize
-Z = (a, b, i, j) => { for(i in a) for(j in a[i]) a[i][j] *= b; return a; }
-
-// Product
-pr = (a, b, i, j) => { for(i in a) for(j in a[i]) a[i][j] *= b[i][j]; return a; }
 
 // Init
 I = (i, h, o) => {
@@ -56,13 +55,12 @@ I = (i, h, o) => {
 // Passthrough
 // P(input, target) // train
 // P(input) // query
-P = (i, t, o, oe) => {
-  o = F(D(w, h = F(D(W, i = C(i)), f)), f);
-  
+P = (i, t, o) => {
+  o = O(O(w, h = O(O(W, i = C(i), 6), f, 5), 6), f, 5);
   if(t){
-    S(t, o);
-    A(w, D(Z(pr(F(o, g), t = C(t)), l), T(h)));
-    A(W, D(Z(pr(F(h, g), D(T(w), t)), l), T(i)));
+    O(t = C(t), o, 2);
+    O(w, O(O(O(O(o, g, 5), t, 3), l, 4), O(h), 6), 1);
+    O(W, O(O(O(O(h, g, 5), O(O(w), t, 6), 3), l, 4), O(i), 6), 1);
   }
   return o.flat();
 }
